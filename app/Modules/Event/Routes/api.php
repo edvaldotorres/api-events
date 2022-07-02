4c88deeeac8491157dab6ce69ceeb96ae42c03be
Edvaldo Torres <edvaldode@gmail.com>
2022-07-02 01:43:23 -0300

    refactor: clear project

--- app/Modules/Event/Routes/api.php
+++ app/Modules/Event/Routes/api.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +13,6 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::middleware('auth:api')->get('/event', function (Request $request) {
+    return $request->user();
+});
--- app/Modules/Event/Routes/web.php
+++ app/Modules/Event/Routes/web.php
@@ -1,7 +1,5 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,3 +10,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+Route::prefix('event')->group(function() {
+    Route::get('/', 'EventController@index');
+});
